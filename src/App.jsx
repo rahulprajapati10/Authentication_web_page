627d4c19681eadb0334284c527ca3cbe58aeb8c9
rahul prajapati <rahulprajapati2930@gmail.com>
2025-02-12 23:23:04 +0530

    main

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import LoginRegister from './Components/LoginRegister/LoginRegister'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <LoginRegister/>
+    </>
+  )
+}
+
+export default App
